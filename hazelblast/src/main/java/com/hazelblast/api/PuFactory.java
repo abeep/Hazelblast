1ff3f2ac4e45a620b338133213ba5428cf71b0c5
Peter Veentjer <alarmnummer@gmail.com>
2012-04-22 22:23:04 +0300

    More cleanup

--- hazelblast/src/main/java/com/hazelblast/api/PuFactory.java
+++ hazelblast/src/main/java/com/hazelblast/api/PuFactory.java
@@ -4,7 +4,7 @@ package com.hazelblast.api;
  * A Factory responsible for creating {@link ProcessingUnit} instances.
  * <p/>
  * By providing different PuFactory implementations, the system can switch between different mechanisms
- * (e.g. Spring or Guice) to create a ProcessingUnit and set up the services to be exposed to the outside
+ * (e.g. Spring or Google-Guice) to create a ProcessingUnit and set up the services to be exposed to the outside
  * world.
  *
  * @author Peter Veentjer.
--- hazelblast/src/test/java/com/hazelblast/server/PuServerTest.java
+++ hazelblast/src/test/java/com/hazelblast/server/PuServerTest.java
@@ -24,7 +24,6 @@ public class PuServerTest {
     // =========================== start =================================
 
 
-
     @Test
     public void unstartedServer() {
         PuServer server = new PuServer(createMock(ProcessingUnit.class));
