59540906a10a34638b9317d145f953ba07fa130c
Peter Veentjer <alarmnummer@gmail.com>
2012-06-05 15:05:34 +0300

    Upgraded to hazelcast 2.1.2

---
+++ hazelblast/src/main/java/com/hazelblast/api/NoOpReducer.java
@@ -0,0 +1,11 @@
+package com.hazelblast.api;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alarmnummer
+ * Date: 6/5/12
+ * Time: 2:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NoOpReducer {
+}
