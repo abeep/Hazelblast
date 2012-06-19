51b336c9bee4ec8be85e1ece6c4b9844b06df653
Peter Veentjer <alarmnummer@gmail.com>
2012-06-19 17:37:11 +0300

    Version of server fixed to 2.0

--- hazelblast/src/main/java/com/hazelblast/server/ServiceContextServer.java
+++ hazelblast/src/main/java/com/hazelblast/server/ServiceContextServer.java
@@ -41,7 +41,7 @@ public final class ServiceContextServer {
         CommandLine commandLine = buildCommandLine(args, options, parser);
 
         if (commandLine.hasOption("version")) {
-            System.out.println("ServiceContextServer version is 0.2-SNAPSHOT");
+            System.out.println("ServiceContextServer version is 0.2");
             System.exit(0);
         }
 
